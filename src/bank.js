49fc028383f6630d20209fc8386203443915f6fb
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-15 17:13:07 +0100

    used push instead of unshift

--- src/bank.js
+++ src/bank.js
@@ -11,18 +11,18 @@ class Bank {
     txn = new Transaction(this.date(), amount, this.balance + amount)
   ) {
     this.balance += amount;
-    this.transactions.unshift(txn);
+    this.transactions.push(txn);
   }
   withdraw(
     amount,
     txn = new Transaction(this.date(), -amount, this.balance - amount)
   ) {
     this.balance -= amount;
-    this.transactions.unshift(txn);
+    this.transactions.push(txn);
   }
   statement() {
     console.log("date || credit || debit || balance");
-    this.transactions.forEach(function (transaction, index) {
+    this.transactions.reverse().forEach(function (transaction, index) {
       console.log(transaction.print());
     });
   }
